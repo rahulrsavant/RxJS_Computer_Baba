644d7a3b6928be4441ef13fc335116b947728b4b
rahul <rahul.rsavant@gmail.com>
2020-12-27 15:11:14 +0530

    Observable setInterval

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import {Observable} from 'rxjs'
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl:'./app.component.html' ,
+  styles: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'RxJs-Computer-baba';
+
+export class AppComponent implements OnInit {
+
+  constructor(){}
+
+ title="title";
+
+  ngOnInit(): void {
+
+
+function Observable(observer:any) {
+  //producer
+  let counter=1;                                                                          
+  const producer=setInterval(()=>{
+    observer.next(counter++);
+  },1000);
+
+  return ()=>{
+      clearInterval(producer);
+  };
+}
+
+const closeFn=Observable({
+    next:(data: any) => console.log(data),
+    error:(err: any) => console.log('error',err),
+    complete:() => console.log('done')    
+  });  
+  
+  setTimeout(()=>{
+    closeFn();
+  },5000);
+}
 }
