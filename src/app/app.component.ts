16ee9840a6f8019e76852c6abcd9c21cd20ec2a2
rahul <rahul.rsavant@gmail.com>
2020-12-28 23:56:43 +0530

    Observable Guard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,20 +18,27 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
 
-
+//=========================== OBSERVABLE ======================================
 const obs=new MyObservable(function subscribe(observer:any) {
   //producer
   let counter=1;                                                                          
-  const producer=setInterval(()=>{
+/*   const producer=setInterval(()=>{
     observer.next(counter++);
   },1000);
+  observer.complete(); */
+
+  observer.next('hello');
   observer.complete();
+  observer.next('world');
 
   return ()=>{
-      clearInterval(producer);
+   //   clearInterval(producer);
   };
 });
 
+
+
+//==================== OBSERVER ==========================================
 const subsription=obs.subscribe({
     next:(data: any) => console.log(data),
     error:(err: any) => console.log('error',err),
@@ -49,16 +56,77 @@ const subsription=obs.subscribe({
 
 
 //======================================================
-class MyObservable{
-  observable:any;
-  
-    constructor(observable:any){
-  this.observable=observable;
+
+  class MyObservableGuard{
+    observer:any;
+    isUnsubscribe:any;
+    closeFn:any;
+    constructor(observer:any){
+      this.observer=observer;
+      this.isUnsubscribe=false;
     }
-  
-    subscribe(observer:any){
-      const closeFn=this.observable(observer);
-      return closeFn;
-    }   
-  
+    next(data:any){
+      if(this.isUnsubscribe || !this.observer.next){
+        return;
+      }
+      
+      try {
+        this.observer.next(data);
+      } catch (error) {
+        this.observer.unsubscribe();
+        throw error;
+      }
+    }
+    error(err:any){
+      if(this.isUnsubscribe || !this.observer.console.error){//if somone not handle error message
+        return;
+      }
+      try{
+      this.observer.error(err);
+      }catch(innerError){
+        this.unsubscribe();
+        throw innerError;
+      }
+      this.observer.unsubscribe();      
+    }
+    complete(){
+      if(this.isUnsubscribe || !this.observer.complete){
+        return;
+      }
+
+      try{
+        this.observer.complete();
+      }catch(err){
+        this.unsubscribe();
+        throw err;
+      }
+      
+      this.unsubscribe();
+    }
+
+    unsubscribe(){
+      this.isUnsubscribe=true;
+      if(this.closeFn)
+      {
+         this.closeFn();
+      }     
+    }
   }
+//==================================================================
+  class MyObservable{
+    observable:any;
+    observer:any;
+    
+      constructor(observable:any){
+    this.observable=observable;
+      }
+    
+      subscribe(observer:any){
+       const myObservableGuard:any=new MyObservableGuard(observer);
+        const closeFn=this.observable(myObservableGuard);
+        myObservableGuard.closeFn=closeFn;
+        return closeFn;
+      }   
+    
+    }
+    //=============================================================
