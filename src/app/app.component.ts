488a5c372f959c95a7c9850ff1ed12787b6f2791
rahul <rahul.rsavant@gmail.com>
2020-12-28 15:22:19 +0530

    "function changes"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import {Observable} from 'rxjs'
+import {Observable} from 'rxjs';
 
 
 @Component({
@@ -8,6 +8,7 @@ import {Observable} from 'rxjs'
   templateUrl:'./app.component.html' ,
   styles: ['./app.component.css']
 })
+
 
 export class AppComponent implements OnInit {
 
@@ -18,26 +19,46 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
 
 
-function Observable(observer:any) {
+const obs=new MyObservable(function subscribe(observer:any) {
   //producer
   let counter=1;                                                                          
   const producer=setInterval(()=>{
     observer.next(counter++);
   },1000);
+  observer.complete();
 
   return ()=>{
       clearInterval(producer);
   };
-}
+});
 
-const closeFn=Observable({
+const subsription=obs.subscribe({
     next:(data: any) => console.log(data),
     error:(err: any) => console.log('error',err),
     complete:() => console.log('done')    
   });  
-  
+
   setTimeout(()=>{
-    closeFn();
+    subsription();
   },5000);
-}
-}
+
+
+  }// ng On init
+
+}// class App component
+
+
+//======================================================
+class MyObservable{
+  observable:any;
+  
+    constructor(observable:any){
+  this.observable=observable;
+    }
+  
+    subscribe(observer:any){
+      const closeFn=this.observable(observer);
+      return closeFn;
+    }   
+  
+  }
